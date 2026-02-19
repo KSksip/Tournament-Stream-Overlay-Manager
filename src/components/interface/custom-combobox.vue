5dfe83ccda539eca729bd396f51bcff1c590a164
Skip <92388016+KSksip@users.noreply.github.com>
2026-02-19 23:31:36 +0100

    FIX: Added case for simpler data returns

--- src/components/interface/custom-combobox.vue
+++ src/components/interface/custom-combobox.vue
@@ -1,9 +1,12 @@
 <template>
-    <div v-if="modelValue" ref="root" class="relative" :class="isFocused ? 'z-50' : ''" >
+    <div ref="root" class="relative" :class="isFocused ? 'z-50' : ''" >
         <div
         :class="inputClass"
         class="z-20 flex border whitespace-nowrap bg-white border-inherit w-full min-w-5">
-            <input v-model="modelValue.name" @click="handleClickedInput()" :placeholder="props.placeholder" type="text" class="outline-none min-w-1 w-full grow placeholder:text-zinc-500">
+            
+            <input v-if="returnName" v-model="modelValue" @click="handleClickedInput()" :placeholder="props.placeholder" type="text" class="outline-none min-w-1 w-full grow placeholder:text-zinc-500">
+            <input v-else v-model="modelValue.name" @click="handleClickedInput()" :placeholder="props.placeholder" type="text" class="outline-none min-w-1 w-full grow placeholder:text-zinc-500">
+            
             <button class="flex" @click="isFocused = !isFocused">
                 <Icon icon="radix-icons:chevron-down" class="my-auto hover:cursor-pointer hover:text-zinc-400"/>
             </button>
@@ -30,11 +33,12 @@
 import { ref, computed, useTemplateRef } from 'vue'
 import { onClickOutside } from '@vueuse/core';
 
-const modelValue = defineModel({type: Object, required: true})
+const modelValue = defineModel({required: true})
 const rootEl = useTemplateRef<HTMLElement>('root')
 const isFocused = ref(false)
     
 const props = defineProps({
+    returnName: Boolean,
     options: Array,
     placeholder: String,
     inputClass: String,
@@ -42,24 +46,43 @@ const props = defineProps({
 })
 
 const filteredOptions = computed(() => {
-    return props.options.filter((option: any) => option.name.toLowerCase().includes(modelValue.value.name.toLowerCase()))
+
+    if(props.returnName){
+        return props.options!.filter((option: any) => option.name.toLowerCase().includes(modelValue.value.toLowerCase()))
+
+    } else {
+        return props.options!.filter((option: any) => option.name.toLowerCase().includes(modelValue.value.name.toLowerCase()))
+    }
+
 })
 
 
 function handleClickedInput(){
     if(!isFocused.value){
-        modelValue.value.name = ''
+        if(props.returnName){
+            modelValue.value = ""
+
+        } else{
+            modelValue.value.name = ""
+        }
     }
     isFocused.value = true
 }
 
 function selectOption(val:any){
     isFocused.value = false
-    modelValue.value = val
+    
+    if(props.returnName){
+        modelValue.value = val.name    
+
+    } else {
+        modelValue.value = val 
+    }
 }
 
 
 onClickOutside(rootEl, (event) => {
+    
     if(isFocused.value){
         isFocused.value = false
     }
